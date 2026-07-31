a64768722084b964a74ae76227627a56e2d065ef
Karen Calderon <krcatari@umsa.bo>
2026-07-30 21:06:52 -0400

    variables

--- app/src/main/java/com/karenkotlin/pruebakoala/variables.kt
+++ app/src/main/java/com/karenkotlin/pruebakoala/variables.kt
@@ -2,4 +2,24 @@ package com.karenkotlin.pruebakoala
 
 fun main(){
     print("holiiii")
+    print("\n")
+    print(plataforma)
 }
+
+// Tipos de Datos
+
+//Kotlin normalmente detecta el tipo
+var lenguaje = "kotlin"
+val plataforma = 23
+
+val nombre:String = "Ana"
+val apellido:String = "jose"
+
+val edad:Int = 22
+var fechaNacimiento:Int = 2002
+
+val altura:Double = 1.65
+var peso:Double = 65.5
+
+val activo:Boolean = true
+var estudiante:Boolean = true
